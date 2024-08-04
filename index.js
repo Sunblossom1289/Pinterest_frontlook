595656fbf38ef44a02d3435d711590adbb834c66
Sunblossom1289 <116423891+Sunblossom1289@users.noreply.github.com>
2024-08-04 19:44:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+var arr= [ {
+    name: "Adventure" ,
+     image : "https://plus.unsplash.com/premium_photo-1663100756975-3316f27f4dda?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8b2xkJTIwdG93bnxlbnwwfHwwfHx8MA%3D%3D"
+},
+{
+    name:"Lilys" , 
+    image :"https://images.unsplash.com/photo-1562764280-9556a02589f6?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGlseXN8ZW58MHx8MHx8fDA%3D"
+},
+
+{
+    name:"aesthetic",
+    image:"https://media.istockphoto.com/id/1212912117/photo/3d-abstracts-background-stock-photo.webp?b=1&s=170667a&w=0&k=20&c=U9kMi4NMcwjllLz7lGl3XkJSKyvZiK-noZp3133C31w="
+},
+
+{
+    name:"cherry blossom",
+    image:"https://images.unsplash.com/photo-1522383225653-ed111181a951?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlcnJ5JTIwYmxvc3NvbXxlbnwwfHwwfHx8MA%3D%3D"
+},
+
+{
+    name:"old town",
+    image:"https://images.unsplash.com/photo-1605096048662-5ab61695a122?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b2xkJTIwdG93bnxlbnwwfHwwfHx8MA%3D%3D"
+},
+{
+    name:"city",
+    image:"https://images.unsplash.com/photo-1514414249854-ac383ccd15ac?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG9sZCUyMHRvd258ZW58MHx8MHx8fDA%3D"
+},
+{
+   name: "Adventure" ,
+     image : "https://plus.unsplash.com/premium_photo-1663100756975-3316f27f4dda?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8b2xkJTIwdG93bnxlbnwwfHwwfHx8MA%3D%3D"
+},
+{
+    name:"Lilys" , 
+    image :"https://images.unsplash.com/photo-1562764280-9556a02589f6?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bGlseXN8ZW58MHx8MHx8fDA%3D"
+},
+
+{
+    name:"aesthetic",
+    image:"https://media.istockphoto.com/id/1212912117/photo/3d-abstracts-background-stock-photo.webp?b=1&s=170667a&w=0&k=20&c=U9kMi4NMcwjllLz7lGl3XkJSKyvZiK-noZp3133C31w="
+},
+
+{
+    name:"cherry blossom",
+    image:"https://images.unsplash.com/photo-1522383225653-ed111181a951?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlcnJ5JTIwYmxvc3NvbXxlbnwwfHwwfHx8MA%3D%3D"
+},
+
+{
+    name:"old town",
+    image:"https://images.unsplash.com/photo-1605096048662-5ab61695a122?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b2xkJTIwdG93bnxlbnwwfHwwfHx8MA%3D%3D"
+},
+{
+    name:"city",
+    image:"https://images.unsplash.com/photo-1514414249854-ac383ccd15ac?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG9sZCUyMHRvd258ZW58MHx8MHx8fDA%3D"
+},{
+    name:"hello",
+    image:"https://media.istockphoto.com/id/1499790229/photo/historic-landmark-hawa-mahal-aka-palace-of-the-winds-in-jaipur-rajasthan-india.webp?b=1&s=170667a&w=0&k=20&c=DLAjcUk3FrRSvA25hscTOwRxD2b3uEhoiMZwzgGdkXE="
+},{
+    name:"little",
+    image:"https://media.istockphoto.com/id/2076531269/photo/aerial-view-of-the-basel-switzerland-skyline.webp?b=1&s=170667a&w=0&k=20&c=0mxBROs2YtJ2QbzMyakhxjF8uN4PXpz79Gx6UGpkxMw="
+},
+{
+    name:"little",
+    image:"https://plus.unsplash.com/premium_photo-1661909918862-403588a10c0b?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8b2xkJTIwdG93bnxlbnwwfHwwfHx8MA%3D%3D"
+},
+{
+    name:"little",
+    image:"https://media.istockphoto.com/id/1492790368/photo/golden-buddha-statue-at-dambulla.webp?b=1&s=170667a&w=0&k=20&c=R9-aAwG1wSNxNx8Bc_e7TyVDHgLCStKbZp8-33_-prE="
+}
+
+
+
+
+]
+
+function showImage(){
+    var pluto ="";
+arr.forEach(function(obj){
+    pluto += `<div class="box">
+    <img class ="cursor-pointer" src="${obj.image}" alt="image">
+   <div class="caption">lorem ispks</div>
+   </div>`;
+
+})
+
+document.querySelector(".container")
+.innerHTML= pluto;
+
+}
+
+function handleSearch(){
+// document.querySelector(".overlay")
+// .style.display ="block";//flex bhi kr skte h 
+var input =document.querySelector("#searchinput");
+input.addEventListener("focus" , function(){
+    document.querySelector(".overlay").style.display ="block";
+})
+
+input.addEventListener("blur" , function(){
+    document.querySelector(".overlay").style.display ="none";
+})
+//array.filter
+
+input.addEventListener("input" ,function(){
+const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
+
+var pluto = "";
+filteredArray.forEach(function(obj){
+    pluto += `<div class="res flex px-8 py-3">
+    <i class ="ri-search-line font-semibold mr-5"></i>
+    <h3 class="font-semibold">${obj.name}</h3>
+    </div>`
+})
+// console.log(filteredArray);
+var cart= document.querySelector(".searchdata");
+cart.style.display="block";
+cart.innerHTML =pluto;
+})
+// cart.addEventListener("blur" , function(){
+//     cart.style.display ="none";
+// })
+
+
+
+}
+
+
+
+showImage();
+handleSearch();
